91bef9968546c43163b2dcb1e9c42caede42b2c2
LesterVASS <lester.potestad@gc.vasslatam.com>
2023-07-09 03:47:10 -0600

    back de proyecto de calculo de grasa corporal  -  inicio y fin

---
+++ Person.cs
@@ -0,0 +1,13 @@
+﻿namespace calculadora_grasa_corporal_back_api
+{
+    public class Person
+    {
+        public String sexo { get; set; }
+        public float peso { get; set; }
+        public int cuello { get; set; }
+        public int cintura { get; set; }
+        public int cadera { get; set; }
+        public int altura { get; set; }
+
+    }
+}
